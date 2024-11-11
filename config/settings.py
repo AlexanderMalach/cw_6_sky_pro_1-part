dc0b02e3bf5d432d9be47ab021514a644caa2751
79096 <a.malakhinsky@gmail.com>
2024-11-11 15:49:37 +0300

    -

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
